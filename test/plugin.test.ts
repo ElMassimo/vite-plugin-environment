04ed88af91e426b23a40ee2da85ab7d91f1df8cf
Maximo Mussini <maximomussini@gmail.com>
2022-07-14 10:53:37 -0300

    chore: update tests

--- test/plugin.test.ts
+++ test/plugin.test.ts
@@ -1,7 +1,8 @@
 import { readFileSync } from 'fs'
 import { join } from 'path'
 import glob from 'fast-glob'
-import { build, InlineConfig } from 'vite'
+import { build } from 'vite'
+import type { InlineConfig } from 'vite'
 import { describe, test, expect } from 'vitest'
 
 type Fixture = 'a' | 'b' | 'c'
@@ -18,85 +19,67 @@ async function buildFixture (name: Fixture, options?: InlineConfig) {
 }
 
 describe('keys', () => {
-  test('throws an error if an environment variable is missing', async (done) => {
-    expect.assertions(1)
+  test('throws an error if an environment variable is missing', async () => {
     try {
       await buildFixture('a', { mode: 'staging' })
     }
     catch (error) {
       expect(error.message).toMatch(/`API_KEY` environment variable is undefined/)
     }
-    done()
   })
 
-  test('replaces the variables', async (done) => {
-    expect.assertions(1)
+  test('replaces the variables', async () => {
     await buildFixture('a', { mode: 'production' })
     expect(compiledApp('a')).toContain('console.log("v2")')
-    done()
   })
 })
 
 describe('default values', () => {
-  test('throws an error if an environment variable is missing', async (done) => {
-    expect.assertions(1)
+  test('throws an error if an environment variable is missing', async () => {
     try {
       await buildFixture('b', { mode: 'staging' })
     }
     catch (error) {
       expect(error.message).toMatch(/`API_KEY` environment variable is undefined/)
     }
-    done()
   })
 
-  test('replaces the variables', async (done) => {
-    expect.assertions(1)
+  test('replaces the variables', async () => {
     await buildFixture('b', { mode: 'production' })
     expect(compiledApp('b')).toContain('console.log("v2")')
-    done()
   })
 
-  test('uses defaults only when not available', async (done) => {
-    expect.assertions(1)
+  test('uses defaults only when not available', async () => {
     await buildFixture('b', { mode: 'development' })
     expect(compiledApp('b')).toContain('console.log("v3")')
-    done()
   })
 })
 
 describe('advanced options', () => {
-  test('throws an error if an environment variable is missing', async (done) => {
-    expect.assertions(1)
+  test('throws an error if an environment variable is missing', async () => {
     try {
       await buildFixture('c', { mode: 'staging' })
     }
     catch (error) {
       expect(error.message).toMatch(/`API_KEY` environment variable is undefined/)
     }
-    done()
   })
 
-  test('replaces the variables without failing', async (done) => {
-    expect.assertions(2)
+  test('replaces the variables without failing', async () => {
     await buildFixture('c', { mode: 'production' })
-    expect(compiledApp('c')).toContain('console.log({}.VUE_APP_VERSION)')
+    expect(compiledApp('c')).toContain('console.log({BASE_URL:"/",MODE:"production",DEV:!1,PROD:!0}.VUE_APP_VERSION)')
     expect(compiledApp('c')).toContain('window.apiKey="d2fab04aacaad208"')
-    done()
   })
 
-  test('uses defaults only when not available', async (done) => {
-    expect.assertions(2)
+  test('uses defaults only when not available', async () => {
     await buildFixture('c', { mode: 'development' })
     expect(compiledApp('c')).toContain('console.log("v3")')
     expect(compiledApp('c')).toContain('window.apiKey="d2fab04aacaad208"')
-    done()
   })
 
-  test('uses envDir when specified', async (done) => {
-    expect.assertions(2)
+  test('uses envDir when specified', async () => {
     await buildFixture('c', { mode: 'development', envDir: 'alt' })
     expect(compiledApp('c')).toContain('console.log("v4")')
     expect(compiledApp('c')).toContain('window.apiKey="c2fab04aacaad2089"')
-    done()
   })
 })
